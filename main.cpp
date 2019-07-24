e3c3e6f5b278fc928614aebfb56d65e48678ee40
Yukino Hayakawa <tennencoll@gmail.com>
2019-07-24 21:29:30 +0800

    new ecs demo

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 ﻿#include <algorithm>
 #include <execution>
 
+#include <fmt/ostream.h>
+
 #include <Usagi/Extension/Win32/Win32.hpp>
 #include <Usagi/Extension/Win32/Win32MacroFix.hpp>
 
 #include <Usagi/Utility/Utf8Main.hpp>
-#include <Usagi/ECS/Game.hpp>
-#include <Usagi/ECS/Entity.hpp>
-#include <Usagi/ECS/GameContext.hpp>
-#include <fmt/ostream.h>
+#include <Usagi/ECS/Game2.hpp>
 
 using namespace usagi::ecs;
 
 struct PositionComponent
 {
     int x = 0, y = 0;
+};
+
+struct FloatValueComponent
+{
+    float value = 0;
 };
 
 struct RGBColorComponent
@@ -24,33 +28,89 @@ struct RGBColorComponent
 
 class CosineWaveValueUpdateSystem : public FrameUpdateSystem
 {
-    Entity::ComponentMask mComponentMask;
-
 public:
-    void init(GameContext &ctx)
+    void execute(GameContext &ctx) override
     {
-        mComponentMask = ctx.entity_manager.buildComponentMask<RGBColorComponent>();
-    }
-
-    void execute() override
-    {
-        // std::for_each(
-        //     std::execution::par,
-        //     ctx.entities.begin(),
-        //     ctx.entities.end(),
-        //     [](auto &&e) {
-        //     if(e.matchMask())
-        // })
+        ctx.execution.parallel<
+            PositionComponent,
+            FloatValueComponent
+        >([&, this](auto &c_pos, auto &c_float_val) {
+            const auto t = static_cast<float>(ctx.master_clock.totalElapsed());
+            c_float_val.value = std::abs(
+                std::cos(c_pos.x + t) +
+                std::cos(c_pos.y * 2 + t)
+            );
+        });
     }
 };
 
+class ColorMappingSystem : public FrameUpdateSystem
+{
+public:
+    void execute(GameContext &ctx) override
+    {
+        ctx.execution.parallel<
+            FloatValueComponent,
+            RGBColorComponent
+        >([this](auto &c_float_val, auto &c_color) {
+            c_color.r = static_cast<int>(c_float_val.value * 255) % 255;
+            c_color.g = static_cast<int>(c_float_val.value * 512) % 255;
+            c_color.b = static_cast<int>(c_float_val.value * 666) % 255;
+        });
+    }
+};
+
+class ConsolePixelDrawSystem : public FrameUpdateSystem
+{
+public:
+    // https://stackoverflow.com/questions/12378642/c-pixels-in-console-window
+    void execute(GameContext &ctx) override
+    {
+        const auto console = GetConsoleWindow();
+        const auto dc = GetDC(console);
+        ctx.execution.sequential<
+            PositionComponent,
+            RGBColorComponent
+        >([&, this](auto &c_pos, auto &c_color) {
+            SetPixel(
+                dc,
+                c_pos.x, c_pos.y,
+                RGB(c_color.r, c_color.g, c_color.b)
+            );
+        });
+        ReleaseDC(console, dc);
+    }
+};
+
+class PixelInitSystem : public GameInitSystem
+{
+public:
+    void execute(GameContext &ctx) override
+    {
+        for(auto i = 0; i < 100; ++i)
+        {
+            for(int j = 0; j < 100; ++j)
+            {
+                ctx.entity_manager.addEntity()
+                    .addComponent<PositionComponent>(i, j)
+                    .addComponent<FloatValueComponent>()
+                    .addComponent<RGBColorComponent>()
+                    ;
+            }
+        }
+    }
+};
 int usagi_main(const std::vector<std::string> &args)
 {
     Game game;
 
-    GameContext ctx;
-    fmt::print("{}\n", ctx.entity_manager.buildComponentMask<RGBColorComponent>());
-    fmt::print("{}\n", ctx.entity_manager.buildComponentMask<RGBColorComponent,PositionComponent>());
+    game.addGameInitSystem(std::make_unique<PixelInitSystem>())
+        .addFrameUpdateSystem(std::make_unique<CosineWaveValueUpdateSystem>())
+        .addFrameUpdateSystem(std::make_unique<ColorMappingSystem>())
+        .addFrameUpdateSystem(std::make_unique<ConsolePixelDrawSystem>())
+        ;
+
+    game.mainLoop();
 
     return 0;
 }
